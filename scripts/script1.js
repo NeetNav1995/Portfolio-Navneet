f34ccfde9faad8f6806c5d3889e6c1b010dd00a2
Navneet <riarnavneet111@gmail.com>
2025-06-15 16:04:17 +1000

    Initial Commit

---
+++ scripts/script1.js
@@ -0,0 +1,25 @@
+
+function addRecommendation() {
+  let el = document.getElementById("message");
+  if (el !== null && el.value != null && el.value.trim() !== "") {
+    let ell = document.createElement("div");
+    ell.setAttribute("class","border-2 border-gray-300 rounded-md recommendation");
+    ell.innerHTML =
+      
+        "\<span\>\<b\>&#8220;\</b\>\</span\>\<b\>"
+       +
+      el.value +
+      "\</b\>\<span\>\<b\>&#8221;\</b\>\</span\>";
+      document.getElementById("all_rec").appendChild(ell);
+    el.value = "";
+    showPopUp();
+  }
+}
+
+function showPopUp(bool=true){
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./htmlFiles/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
